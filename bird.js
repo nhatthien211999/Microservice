69c1ef0592d81e09b110947f82222d4cb58bd460
Your Name <you@example.com>
2021-06-19 17:15:39 +0700

    first commit

---
+++ bird.js
@@ -0,0 +1,178 @@
+const uri = 'http://nhatthien.gear.host/api/birds';
+const page_load = () => {
+
+    try {
+       fetch(uri)
+      .then((response) => response.json())
+      .then(data => {
+            renderBirdList(data);
+        })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+
+    } catch (error) {
+        console.log(error);
+    }
+    
+
+}
+
+const btnSearch_Click = () => {
+    let keyword = document.getElementById("txtKeyword").value.trim();
+    if (keyword.length > 0)
+      search(keyword);
+    else
+    page_load();
+}
+
+const btnAdd_click = () => {
+    const newBird = {
+        code: document.getElementById("txtCode").value,
+        name: document.getElementById("txtName").value,
+        price: document.getElementById("txtPrice").value,
+        information: document.getElementById("txtInfo").value,
+        type: document.getElementById("txtType").value,
+    };
+    addNewBird(newBird);
+}
+
+const btnUpdate_click = () => {
+    const newBird = {
+        code: document.getElementById("txtCode").value,
+        name: document.getElementById("txtName").value,
+        price: document.getElementById("txtPrice").value,
+        information: document.getElementById("txtInfo").value,
+        type: document.getElementById("txtType").value,
+    };
+
+    updateBird(newBird);
+
+}
+
+const btnDelete_click = () => {
+    let code = document.getElementById("txtCode").value;
+    deleteBird(code);
+}
+
+function renderBirdList(birds) {
+    console.log(birds);
+    var rows = "";
+    for (var bird of birds) {
+      rows += "<tr>";
+      rows += "<td><a href='#' onclick='lnkID_Click(" + bird.Code + ")'>" + bird.Code + "</a></td>";
+      rows += "<td>" + bird.Name + "</td>";
+      rows += "<td>" + bird.Price + "</td>";
+      rows += "<td>" + bird.Information + "</td>";
+      rows += "<td>" + bird.Type + "</td>";
+      rows += "</tr>";
+    }
+    var header = "<tr><th>Code</th><th>Name</th><th>Price</th><th>Information</th><th>Type</th></tr>";
+  
+    document.getElementById("lists").innerHTML = header + rows;
+  }
+
+  function renderBirdDetails(bird) {
+    document.getElementById("txtCode").value = bird.Code;
+    document.getElementById("txtName").value = bird.Name;
+    document.getElementById("txtPrice").value = bird.Price;
+    document.getElementById("txtInfo").value = bird.Information;
+    document.getElementById("txtType").value = bird.Type;
+  }
+
+function getDetails(code) {
+    fetch(uri + '/' + code)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        renderBirdDetails(data);
+    });
+}
+
+function lnkID_Click(code) {
+    getDetails(code);
+}
+
+const addNewBird = (newBird) => {
+    fetch(uri, {
+        method: 'POST', // or 'PUT'
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newBird),
+    })
+    .then(response => {
+        console.log(response);
+        document.getElementById("err").innerHTML = "";
+        document.getElementById("success").innerHTML = "<b>Thêm thành công</b>";
+        page_load();
+    })
+    .catch(error => {
+        console.log(error);
+        document.getElementById("success").innerHTML = "";
+        document.getElementById("message").innerHTML = "<b>Thêm thất bại</b>";
+    })
+}
+
+const updateBird = (newBird) => {
+    fetch(uri + "/" + newBird.Code, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newBird),
+    })
+    .then(response => {
+        console.log(response);
+        document.getElementById("err").innerHTML = "";
+        document.getElementById("success").innerHTML = "<b>Cập nhập thành công</b>";
+        page_load();
+    })
+    .catch(error => {
+        console.log(error);
+        document.getElementById("success").innerHTML = "";
+        document.getElementById("err").innerHTML = "<b>Cập nhập thất bại</b>";
+    })
+}
+
+const deleteBird = (code) => {
+    fetch(uri + "/" + code, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+    })
+    .then(response => {
+        console.log(response);
+        document.getElementById("err").innerHTML = "";
+        document.getElementById("success").innerHTML = "<b>Xóa thành công</b>";
+        page_load();
+        resetForm();
+    })
+    .catch(error => {
+        console.log(error);
+        document.getElementById("success").innerHTML = "";
+        document.getElementById("err").innerHTML = "<b>Xóa thất bại</b>";
+    })
+}
+
+const search = (keyword) => {
+    fetch(uri + "/search/" + keyword, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        renderBirdList(data);
+    });
+}
+
+const resetForm = () => {
+    document.getElementById("txtCode").value = "";
+    document.getElementById("txtName").value = "";
+    document.getElementById("txtPrice").value = "";
+    document.getElementById("txtInfo").value = "";
+    document.getElementById("txtType").value = "";
+}
---
+++ main.js
@@ -0,0 +1,104 @@
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyBLYty07Sjrku__ysTND6HbuB5MVhesAt4",
+        authDomain: "ktpm-4a77d.firebaseapp.com",
+        databaseURL: "https://ktpm-4a77d-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "ktpm-4a77d",
+        storageBucket: "ktpm-4a77d.appspot.com",
+        messagingSenderId: "415093263193",
+        appId: "1:415093263193:web:40fc16b3cb85b4b3a91a48"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+  /* helper methods */
+
+const dbRef = firebase.database().ref();
+
+const btnLogin_click = async () =>{
+    const user = {
+        'email': document.getElementById('txtEmail').value,
+        'password': document.getElementById('txtPassword').value
+    }
+    if(await check_login(user)){
+        window.location="http://127.0.0.1:5500/Bird.html";
+    }
+    else{
+        document.getElementById("err").innerHTML = "<b>Đăng Nhập Thất Bại</b>";
+    }
+}
+
+const btnRegister_click = () =>{
+    const user = {
+        'email': document.getElementById('txtEmail').value,
+        'password': document.getElementById('txtPassword').value
+    }
+
+    console.log(user);
+    addNew(user);
+}
+
+const addNew = async (newUser) => {
+
+    if(await check_email(newUser.email)){
+        try{
+            document.getElementById("err").innerHTML = "";
+            document.getElementById("success").innerHTML = "<b>Đăng ký thành công</b>";
+            dbRef.child("users").push(newUser);
+        }
+        catch(err){
+            console.log(err);
+            document.getElementById("success").innerHTML = "";
+            document.getElementById("err").innerHTML = "<b>Đăng ký thất bại</b>";
+        }
+    }else{
+        document.getElementById("success").innerHTML = "";
+        document.getElementById("err").innerHTML = "<b>Đăng ký thất bại</b>";
+    }
+}
+
+const check_email = async (email) => {
+    let key = '';
+    await dbRef.child("users").once("value", (snapshot) => {
+      snapshot.forEach(element => {
+        let user = element.val();
+        if(user.email === email){
+            key = element.key;
+            console.log('for: ' + key);
+        }
+      });
+    });
+    
+    console.log('after ' + key);
+
+    if(key !== ''){
+        console.log(key);
+        return false;
+    }else{
+        return true;
+    }   
+   
+}
+const check_login = async (userLogin) => {
+    let key = '';
+    await dbRef.child("users").once("value", (snapshot) => {
+      snapshot.forEach(element => {
+        let user = element.val();
+        if(user.email === userLogin.email && user.password === userLogin.password){
+            key = element.key;
+            console.log('for: ' + key);
+        }
+      });
+    });
+    
+    console.log('after ' + key);
+
+    if(key === ''){
+        
+        return false;
+    }else{
+
+        return true;
+    }   
+   
+}
+
